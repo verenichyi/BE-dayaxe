3b6e2365e9dd7cdf02cf6b447551242d3fcf97d1
verenichyi <verenichyi@gmail.com>
2023-03-06 15:02:55 +0300

    init: add mongodb database with mongoose

---
+++ src/entities/users/users.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from './user.schema';
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+  async getAll(): Promise<User[]> {
+    return this.userModel.find();
+  }
+}

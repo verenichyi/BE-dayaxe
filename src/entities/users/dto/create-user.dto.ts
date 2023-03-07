1b84b7f6c269304177a965cc2a18c1f55e9cbefe
Nikolay <nikolay.kovzik@gmail.com>
2023-03-07 18:58:17 +0300

    add valdation

--- src/entities/users/dto/create-user.dto.ts
+++ src/entities/users/dto/create-user.dto.ts
@@ -1,16 +1,21 @@
-import { IsEmail, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
   @ApiProperty()
   @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
   name: string;
 
   @ApiProperty()
+  @IsNotEmpty()
   @IsString()
+  @MinLength(4)
   password: string;
 
   @ApiProperty()
+  @IsNotEmpty()
   @IsEmail()
   email: string;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from './pipes/validation.pipe';
 
 async function bootstrap() {
   const PORT = process.env.PORT || 4000;
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder().setTitle('Dayaxe CMS').build();
   const document = SwaggerModule.createDocument(app, config);

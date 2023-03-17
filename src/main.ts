ed909b3cd01ccf69e9e1a6f49562d64f219bdbfc
verenichyi <verenichyi@gmail.com>
2023-03-17 12:45:25 +0300

    feat: set body size limit

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
 import { ValidationPipe } from './pipes/validation.pipe';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const PORT = process.env.PORT || 4000;
@@ -9,6 +10,8 @@ async function bootstrap() {
 
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ limit: '50mb' }));
 
   const config = new DocumentBuilder()
     .setTitle('Dayaxe CMS')
